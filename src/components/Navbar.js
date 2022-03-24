e76d9337d5ba032dfbe7cc7f1cd55e889ab430ae
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-03-24 23:11:44 +0900

    Added content to hero page forgot to make new branch

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,7 @@ export default function Navbar() {
     return (
         <nav>
             <img src={Logo} className="nav--logo" alt="Airbnb logo" />
+            <h1>Online Experiences</h1>
         </nav>
     )
 }
