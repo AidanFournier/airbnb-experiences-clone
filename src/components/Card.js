42571a39ebd4f103d5f02d14787053eb1695ce95
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-04-08 16:06:51 +0900

    Spread object as props

--- src/components/Card.js
+++ src/components/Card.js
@@ -13,15 +13,15 @@ export default function Card(props) {
     return (
         <div className="card">
             {badgeText && <div className="card--badge">{badgeText}</div>}
-            <img src={`../${props.data.coverImg}`} className="card--image" />
+            <img src={`../${props.coverImg}`} className="card--image" />
             <div className="card--stats">
                 <img src={Star} className="card--star" />
                 <span>{props.rating}</span>
-                <span className="gray">({props.data.stats.reviewCount}) • </span>
-                <span className="gray">{props.data.location}</span>
+                <span className="gray">({props.stats.reviewCount}) • </span>
+                <span className="gray">{props.location}</span>
             </div>
-            <p className="card--title">{props.data.title}</p>
-            <p className="card--price"><span className="bold">From ${props.data.price}</span> / person</p>
+            <p className="card--title">{props.title}</p>
+            <p className="card--price"><span className="bold">From ${props.price}</span> / person</p>
         </div>
     )
 }
