63386ff5875f45c9680d836e98180c16c7c08ff2
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-04-08 15:20:25 +0900

    Mapped experiences data into card components

--- src/App.js
+++ src/App.js
@@ -2,20 +2,28 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
+import cardData from "./data"
 
 export default function App() {
+    const cardElements = cardData.map(data => 
+            <Card 
+                id={data.id}
+                title={data.title}
+                description={data.description}
+                price={data.price}
+                img={data.coverImg}
+                reviewCount={data.stats.reviewCount}
+                rating={data.stats.rating}
+                location={data.location}
+                openSpots={data.openSpots}
+            />
+        )
+
     return (
         <div>
             <Navbar />
             <Hero />
-            <Card 
-                img="katie-zaferes.png"
-                rating="5.0"
-                reviewCount={6}
-                country="USA"
-                title="Life Lessons with Katie Zaferes"
-                price={136}
-            />
+            {cardElements}
         </div>
     )
 }
