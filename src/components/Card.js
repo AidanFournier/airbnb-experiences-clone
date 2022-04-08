245849d9310aacacd0931a4afe582d0e4c2820a2
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-04-08 12:54:32 +0900

    Passed props to Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,18 @@
 import React from "react"
-import Katie from "../images/katie-zaferes.png"
 import Star from "../images/star.png"
 
-export default function Card() {
+export default function Card(props) {
     return (
         <div className="card">
-            <img src={Katie} className="card--image" alt="Swimmer Katie Zaferes"/>
+            <img src={`../images/${props.img}`}className="card--image" alt="Swimmer Katie Zaferes" />
             <div className="card--stats">
                 <img src={Star} className="card--star" alt="pink star"/>
-                <span>5.0</span>
-                <span className="gray">(6) • </span>
-                <span className="gray">USA</span>
+                <span>{props.rating}</span>
+                <span className="gray">({props.reviewCount}) • </span>
+                <span className="gray"> {props.country}</span>
             </div>
-            <p>Life Lessons with Katie Zaferes</p>
-            <p><span className="bold">From $136</span> / person</p>
+            <p>{props.title}</p>
+            <p><span className="bold">From ${props.price}</span> / person</p>
         </div>
     )
 }
